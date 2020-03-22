be44e9b5b65a7d1dcca6cd73ec26c1490e5ff6b8
Neal Vos <info@nealvos.nl>
2020-03-22 16:53:49 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,37 @@
 module.exports = [{
-  entry: './app.scss',
-  output: {
-    // This is necessary for webpack to compile
-    // But we never use style-bundle.js
-    filename: 'style-bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'bundle.css',
-            },
+	entry: './app.scss',
+	output: {
+		// This is necessary for webpack to compile
+		// But we never use style-bundle.js
+		filename: 'style-bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'bundle.css',
+						},
           },
-          { loader: 'extract-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              // Prefer Dart Sass
-              implementation: require('sass'),
-            },
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							// Prefer Dart Sass
+							implementation: require('sass'),
+							includePaths: glob.sync('packages/*/node_modules').map((d) => path.join(__dirname, d)),
+						},
           },
         ]
       }
     ]
-  },
+	},
 }];
