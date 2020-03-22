573e5284deffd8351b66104d81155dd0d420a041
Neal Vos <info@nealvos.nl>
2020-03-22 22:05:48 +0100

    Added most of the things
    Wow. Never thought I would almost finish all of it in a couple of hours.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = [{
-	entry: './app.scss',
+	entry: ['./app.scss', './app.js'],
 	output: {
 		// This is necessary for webpack to compile
 		// But we never use style-bundle.js
-		filename: 'style-bundle.js',
+		filename: 'bundle.js',
 	},
 	module: {
 		rules: [
@@ -29,10 +29,12 @@ module.exports = [{
 						options: {
 							// Prefer Dart Sass
 							implementation: require('sass'),
-						},
+							webpackImporter: false,
+              					sassOptions: {
+                					includePaths: ['node_modules'],
+								},
           },
-        ]
       }
-    ]
-	},
+    ]},
+]},
 }];
